c62ad9a04490c3e916d08fcc50b7ff92167deb6b
Prince <princekili0422@gmail.com>
2020-11-28 16:45:21 +0800

    Customize the user location annotation.

--- Yogogo/Yogogo/Controllers/Maps/MapVC.swift
+++ Yogogo/Yogogo/Controllers/Maps/MapVC.swift
@@ -2,9 +2,9 @@
 //  ViewController.swift
 //  Yogogo
 //
-//  Created by prince on 2020/11/26.
+//  Created by prince on 2020/11/28.
 //
-//  MapsVC is responsible for showing location of users' online friends.
+//  MapsVC is responsible for showing location of online users.
 
 import UIKit
 import Mapbox
@@ -12,75 +12,138 @@ import Mapbox
 class MapVC: UIViewController, MGLMapViewDelegate {
     
     var mapView: MGLMapView?
-    var preciseButton: UIButton?
+    
+//    let point = MGLPointAnnotation()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
-//        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        
+        setupMapView()
+    }
+    
+    func setupMapView() {
         let mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        let latitude = 25.042523125298583
-        let longitude = 121.5648858334537
-        mapView.setCenter(CLLocationCoordinate2D(latitude: latitude, longitude: longitude), zoomLevel: 24, animated: true)
         mapView.delegate = self
-        mapView.showsUserLocation = true
-        self.mapView = mapView
 
+        // Enable heading tracking mode so that the arrow will appear.
+        mapView.userTrackingMode = .followWithHeading
+
+        // Enable the permanent heading indicator, which will appear when the tracking mode is not `.followWithHeading`.
+        mapView.showsUserHeadingIndicator = true
+
+        self.mapView = mapView
         view.addSubview(mapView)
     }
 
-    /**
-        In order to enable the alert that requests temporary precise location,
-        please add the following key to your info.plist
-        `NSLocationTemporaryUsageDescriptionDictionary`
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+        // Substitute our custom view for the user location annotation. This custom view is defined below.
+        if annotation is MGLUserLocation && mapView.userLocation != nil {
+            return CustomUserLocationAnnotationView()
+        }
+        return nil
+    }
 
-        You must then add
-        `MGLAccuracyAuthorizationDescription`
-        as a key in the Privacy - Location Temporary Usage Description Dictionary
-     */
-    @available(iOS 14, *)
-    func mapView(_ mapView: MGLMapView, didChangeLocationManagerAuthorization manager: MGLLocationManager) {
-        guard let accuracySetting = manager.accuracyAuthorization?() else { return }
-
-        if accuracySetting == .reducedAccuracy {
-            addPreciseButton()
+    // Optional: tap the user location annotation to toggle heading tracking mode.
+    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
+        if mapView.userTrackingMode != .followWithHeading {
+            mapView.userTrackingMode = .followWithHeading
         } else {
-            removePreciseButton()
+            mapView.resetNorth()
+        }
+
+        // We're borrowing this method as a gesture recognizer, so reset selection state.
+        mapView.deselectAnnotation(annotation, animated: false)
+    }
+}
+
+    // MARK: - MGLUserLocationAnnotationView
+
+// Create a subclass of MGLUserLocationAnnotationView.
+class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
+    
+    let size: CGFloat = 48
+    var dot: CALayer!
+    var arrow: CAShapeLayer!
+
+    // `update` is a method inherited from MGLUserLocationAnnotationView. It updates the appearance of the user location annotation when needed. This can be called many times a second, so be careful to keep it lightweight.
+    override func update() {
+        if frame.isNull {
+            frame = CGRect(x: 0, y: 0, width: size, height: size)
+            return setNeedsLayout()
+        }
+
+        // Check whether we have the user’s location yet.
+        if CLLocationCoordinate2DIsValid(userLocation!.coordinate) {
+            setupLayers()
+            updateHeading()
         }
     }
 
-    @available(iOS 14, *)
-    func addPreciseButton() {
-        let preciseButton = UIButton(frame: CGRect.zero)
-        preciseButton.setTitle("Turn Precise On", for: .normal)
-        preciseButton.backgroundColor = .gray
+    private func updateHeading() {
+        // Show the heading arrow, if the heading of the user is available.
+        if let heading = userLocation!.heading?.trueHeading {
+            arrow.isHidden = false
 
-        preciseButton.addTarget(self, action: #selector(requestTemporaryAuth), for: .touchDown)
-        self.view.addSubview(preciseButton)
-        self.preciseButton = preciseButton
+            // Get the difference between the map’s current direction and the user’s heading, then convert it from degrees to radians.
+            let rotation: CGFloat = -MGLRadiansFromDegrees(mapView!.direction - heading)
 
-        // constraints
-        preciseButton.translatesAutoresizingMaskIntoConstraints = false
-        preciseButton.widthAnchor.constraint(equalToConstant: 150.0).isActive = true
-        preciseButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
-        preciseButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100.0).isActive = true
-        preciseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            // If the difference would be perceptible, rotate the arrow.
+            if abs(rotation) > 0.01 {
+                // Disable implicit animations of this rotation, which reduces lag between changes.
+                CATransaction.begin()
+                CATransaction.setDisableActions(true)
+                arrow.setAffineTransform(CGAffineTransform.identity.rotated(by: rotation))
+                CATransaction.commit()
+            }
+        } else {
+            arrow.isHidden = true
+        }
     }
 
-    @available(iOS 14, *)
-    @objc private func requestTemporaryAuth() {
-        guard let mapView = self.mapView else { return }
+    private func setupLayers() {
+        // This dot forms the base of the annotation.
+        if dot == nil {
+            dot = CALayer()
+            dot.bounds = CGRect(x: 0, y: 0, width: size, height: size)
 
-        let purposeKey = "MGLAccuracyAuthorizationDescription"
-        mapView.locationManager.requestTemporaryFullAccuracyAuthorization!(withPurposeKey: purposeKey)
+            // Use CALayer’s corner radius to turn this layer into a circle.
+            dot.cornerRadius = size / 2
+            dot.backgroundColor = super.tintColor.cgColor
+            dot.borderWidth = 4
+            dot.borderColor = UIColor.white.cgColor
+            layer.addSublayer(dot)
+        }
+
+        // This arrow overlays the dot and is rotated with the user’s heading.
+        if arrow == nil {
+            arrow = CAShapeLayer()
+            arrow.path = arrowPath()
+            arrow.frame = CGRect(x: 0, y: 0, width: size / 2, height: size / 2)
+            arrow.position = CGPoint(x: dot.frame.midX, y: dot.frame.midY)
+            arrow.fillColor = dot.borderColor
+            layer.addSublayer(arrow)
+        }
     }
 
-    private func removePreciseButton() {
-        guard let button = self.preciseButton else { return }
-        button.removeFromSuperview()
-        self.preciseButton = nil
+    // Calculate the vector path for an arrow, for use in a shape layer.
+    private func arrowPath() -> CGPath {
+        let max: CGFloat = size / 2
+        let pad: CGFloat = 3
+
+        let top =    CGPoint(x: max * 0.5, y: 0)
+        let left =   CGPoint(x: 0 + pad,   y: max - pad)
+        let right =  CGPoint(x: max - pad, y: max - pad)
+        let center = CGPoint(x: max * 0.5, y: max * 0.6)
+
+        let bezierPath = UIBezierPath()
+        bezierPath.move(to: top)
+        bezierPath.addLine(to: left)
+        bezierPath.addLine(to: center)
+        bezierPath.addLine(to: right)
+        bezierPath.addLine(to: top)
+        bezierPath.close()
+
+        return bezierPath.cgPath
     }
 }
