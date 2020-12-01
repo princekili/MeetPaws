67ad41245a049ef2ec274b4b03e718c37b40dbb6
Prince <princekili0422@gmail.com>
2020-12-01 22:46:18 +0800

    Modify storyboard layouts

---
+++ Yogogo/Yogogo/Helper Tools/UILabel + Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UILabel + Extension.swift
+//  Yogogo
+//
+//  Created by prince on 2020/12/1.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    var textCount: Int {
+        
+        return countLabelLines()
+    }
+    
+    func countLabelLines() -> Int {
+        
+            // Call self.layoutIfNeeded() if your view uses auto layout
+            let myText = self.text! as NSString
+        
+            let rect = CGSize(width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        
+            let labelSize = myText.boundingRect(
+                with: rect,
+                options: .usesLineFragmentOrigin,
+                attributes: [NSAttributedString.Key.font: self.font!],
+                context: nil
+            )
+        
+            return Int(ceil(CGFloat(labelSize.height) / self.font.lineHeight))
+        }
+}
+
