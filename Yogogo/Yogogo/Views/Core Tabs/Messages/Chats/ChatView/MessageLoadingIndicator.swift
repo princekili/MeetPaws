1e9c9ded81071087d684ceb7999e16653d4c8797
Prince <princekili0422@gmail.com>
2020-12-20 22:18:50 +0800

    Add MessageLoadingIndicator

---
+++ Yogogo/Yogogo/Views/Core Tabs/Messages/Chats/ChatView/MessageLoadingIndicator.swift
@@ -0,0 +1,50 @@
+//
+//  MessageLoadingIndicator.swift
+//  Insdogram
+//
+//  Created by prince on 2020/12/20.
+//
+
+import UIKit
+
+class MessageLoadingIndicator: UIActivityIndicatorView {
+
+    var const: CGFloat!
+    
+    var chatVC: ChatVC!
+    
+    var order: Bool!
+    
+    // MARK: -
+    
+    init(frame: CGRect, const: CGFloat, chatVC: ChatVC) {
+        super.init(frame: frame)
+        self.const = const
+        self.chatVC = chatVC
+        setupIndicator()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: -
+    
+    private func setupIndicator(){
+        hidesWhenStopped = true
+        var topConst: CGFloat = 90
+        if const == 8 {
+            topConst = 70
+        }
+        color = .black
+        chatVC.view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            centerXAnchor.constraint(equalTo: chatVC.view.centerXAnchor),
+            topAnchor.constraint(equalTo: chatVC.view.topAnchor, constant: topConst),
+            widthAnchor.constraint(equalToConstant: 25),
+            heightAnchor.constraint(equalToConstant: 25)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
