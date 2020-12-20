67c4206d26abe123e540c8e3d67776ba4b23a1c0
Prince <princekili0422@gmail.com>
2020-12-20 22:20:13 +0800

    Add UserResponse

---
+++ Yogogo/Yogogo/Views/Core Tabs/Messages/Chats/ResponseView/UserResponse.swift
@@ -0,0 +1,33 @@
+//
+//  UserResponse.swift
+//  Insdogram
+//
+//  Created by prince on 2020/12/20.
+//
+
+import UIKit
+
+class UserResponse {
+    
+    var responseStatus = false
+    
+    var repliedMessage: Messages?
+    
+    var messageToForward: Messages?
+    
+    var messageSender: String?
+    
+    let lineView = UIView()
+    
+    let nameLabel = UILabel()
+    
+    var nameLabelConstraint: NSLayoutConstraint!
+    
+    let messageLabel = UILabel()
+    
+    let mediaMessage = UIImageView()
+    
+    let audioMessage = UILabel()
+    
+    let exitButton = UIButton(type: .system)
+}
